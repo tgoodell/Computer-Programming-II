ea23e15854412194c0c19e96752d6507f92954c1
Laptop <tgoodell@disroot.org>
2020-09-23 19:09:09 -0500

    34

--- A02/guigumdrops.java
+++ A02/guigumdrops.java
@@ -6,7 +6,7 @@ import javax.swing.Timer;
 public class guigumdrops extends JPanel
 {
 	public static int winWidth=1920;
-	public static int winHeight=1080;
+	public static int winHeight=1000;
 	
     public static void main(String[] args)
     {
@@ -42,10 +42,15 @@ class MyCustomPanel extends JPanel implements MouseMotionListener, KeyListener
     double vx=0;
     double vy=0;
     double gravity=0.8;
-    double drag=0.2;
+    double drag=0.1;
+    boolean leftPressed=false;
+    boolean rightPressed=false;
+    boolean upPressed=false;
     
     int winWidth=1920;
-    int winHeight=1080;
+    int winHeight=1000;
+    
+    int frameCount=0;
     
     boolean onGround=false;
     
@@ -59,19 +64,26 @@ class MyCustomPanel extends JPanel implements MouseMotionListener, KeyListener
     
     public void refreshScreen() 
     {
-		timer = new Timer(0, new ActionListener() {
+		
+		timer = new Timer(0, new ActionListener() 
+		{
 		@Override
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent e) 
+			{
+				// Stop Jump Velocity
+				if(vy==-2*gravity)
+				{
+					vy=0;
+				}
 				
 				// Vertical Border Stops
 				if(y>winHeight-80)
 				{
 					y=winHeight-80;
-					//vy=-.3*vy;
-					vy=0;
+					vy=-0.99*vy;
 				}
 				
-				if(y<0 && vy>0)
+				if(y<0)
 				{
 					y=0;
 				}
@@ -86,13 +98,29 @@ class MyCustomPanel extends JPanel implements MouseMotionListener, KeyListener
 				}
 				
 				// Terminal Velocity
-				if(vy>5)
-				{
-					vy=0;
-				} else if(vy<-5)
-				{
-					vy=0;
-				}
+				//~ if(vy>5)
+				//~ {
+					//~ vy=0;
+				//~ } else if(vy<-5)
+				//~ {
+					//~ vy=0;
+				//~ }
+				
+				// Horizontal Max Speed
+				//~ if(vx>10)
+				//~ {
+					//~ x+=10;
+					//~ vx=10;
+				//~ } else if(vx<-10)
+				//~ {
+					//~ x-=10;
+					//~ vx=-10;
+				//~ }
+				//~ else
+				//~ {
+					//~ x+=vx;
+				//~ }
+				x+=vx;
 				
 				// Horizontal Drag
 				if(vx>0)
@@ -105,32 +133,31 @@ class MyCustomPanel extends JPanel implements MouseMotionListener, KeyListener
 					vx+=drag;
 				}
 				
-				// Horizontal Max Speed
-				if(vx>5)
+				// First platform collison
+				
+				if(x>(winWidth/4) && x<(winWidth/4)+300 && y==winHeight-280)
 				{
-					x+=5;
-					vx=5;
-				} else if(vx<-5)
-				{
-					x-=5;
-					vx=-5;
-				}
-				else
-				{
-					x+=vx;
+					y=winHeight-280;
+					vy=0;
 				}
 				
-				addMouseListener(new MouseAdapter() {
-					@Override
-					public void mouseClicked(MouseEvent e) {
-						y-=2*gravity;
-					}
-				});
+				if(leftPressed)vx-=0.5;
+				if(rightPressed)vx+=0.5;
+				if(upPressed)vy-=2*gravity;
 				
 				vy+=gravity;
 				y+=vy;
 				sx=x+20;
 				sy=y+20;
+				
+				//~ addMouseListener(new MouseAdapter() {
+					//~ @Override
+					//~ public void mouseClicked(MouseEvent e) {
+						//~ vy-=2*gravity;
+					//~ }
+				//~ });
+				
+				//frameCount++;
 				
 				revalidate();
 				repaint();
@@ -153,6 +180,7 @@ class MyCustomPanel extends JPanel implements MouseMotionListener, KeyListener
     
     public void paint(Graphics g){
         Graphics2D g2=(Graphics2D)g;
+        
         g2.setColor(Color.WHITE);
         g2.setStroke(new BasicStroke(10));
         g2.fillRect(0, 0, winWidth, winHeight); 
@@ -160,36 +188,40 @@ class MyCustomPanel extends JPanel implements MouseMotionListener, KeyListener
         g2.setColor(Color.BLACK);
         super.paintComponent(g2);  
         g2.drawString((int)sx + ", " + (int)sy, 20,20);
-        g2.drawString(String.valueOf(onGround), 20,40);
+        g2.drawString((int)vx + ", " + (int)vy, 20,40);
         
         g2.setStroke(new BasicStroke(10));
         g2.fillRect(0, winHeight-40, winWidth, winHeight); 
         
+        //mid platform
+        g2.fillRect(winWidth/4,winHeight-200,300,40); 
+        
         g2.setColor(Color.CYAN);
         g2.setStroke(new BasicStroke(10));
         
-        g2.fillRect((int)x, (int)y, 40, 40);
+        //g2.fillRect((int)(x+5*Math.sin(2*Math.PI*frameCount/15)), (int)(y+5*Math.sin(2*Math.PI*frameCount/11)), 40, 40);
+        g2.fillRect((int)x,(int)y,40,40);
+        
+        g2.setColor(randomColor());
+        g2.setStroke(new BasicStroke(1));
+        g2.fillRect((int)sx,(int)sy,4,4);
 
     }
     
-    public void act(int keyNum)
+    public void keyReleased(KeyEvent e)
     {
-		int keyCode=keyNum;
-		if(keyCode==KeyEvent.VK_LEFT)
-		{
-			vx-=0.1;
-		} else if(keyCode==KeyEvent.VK_RIGHT)
-		{
-			vx+=0.1;
-		}
+		int keyCode=e.getKeyCode();
+		if(keyCode==KeyEvent.VK_LEFT || keyCode==KeyEvent.VK_A) leftPressed=false;
+		if(keyCode==KeyEvent.VK_RIGHT || keyCode==KeyEvent.VK_D) rightPressed=false;
+		if(keyCode==KeyEvent.VK_UP || keyCode==KeyEvent.VK_W) upPressed=false;
 	}
-    
-    public void keyReleased(KeyEvent e){}
     
 	public void keyPressed(KeyEvent e)
 	{
 		int keyCode=e.getKeyCode();
-		act(keyCode);
+		if(keyCode==KeyEvent.VK_LEFT || keyCode==KeyEvent.VK_A) leftPressed=true;
+		if(keyCode==KeyEvent.VK_RIGHT || keyCode==KeyEvent.VK_D) rightPressed=true;
+		if(keyCode==KeyEvent.VK_UP || keyCode==KeyEvent.VK_W) upPressed=true;
 	}
 	
 	public void keyTyped(KeyEvent e){}
